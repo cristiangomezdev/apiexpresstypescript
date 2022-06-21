1aee86d5738fa9afe3de23bf2ed731ca69277ad0
cristian <cristiangomez.dev@gmail.com>
2022-06-21 03:29:29 -0300

    settings

---
+++ src/services/diaryServices.ts
@@ -0,0 +1,5 @@
+import diaryData from './diaries.json';
+import { DiaryEntry } from '../types';
+const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>;
+export const getEntries = () => diaryData;
+export const addEntry = () => null;
